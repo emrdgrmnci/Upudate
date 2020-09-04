f8a437a6cea1e8183de7511dd09d39cc28be5897
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-09-04 21:08:59 +0300

    Add navigation bar items

--- Upudate/Modules/Main/MainViewController.swift
+++ Upudate/Modules/Main/MainViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 import SnapKit
 
+enum BarButtonTitle: String {
+    case photoLibrary = "Photo Library"
+    case camera = "Camera"
+}
+
 class MainViewController: UIViewController {
+
+    lazy var box = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .red
-        
+
+        //        self.view.addSubview(box)
+        //        box.backgroundColor = .green
+        //        box.image = UIImage(named: "a")
+        //        box.snp.makeConstraints { (make) -> Void in
+        //            make.width.height.equalTo(50)
+        //            make.center.equalTo(self.view)
+        //        }
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: BarButtonTitle.photoLibrary.rawValue, style: .plain, target: self, action: #selector(addTapped))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: BarButtonTitle.camera.rawValue, style: .plain, target: self, action: #selector(addTapped))
+    }
+
+    @objc func addTapped() {
+        print("bars tapped")
     }
 }
