ab5ce1b7a0cf522b5a466e7eb1f9cc40fa63300a
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-09-04 18:47:05 +0300

    Add group folders and create MainViewController file

---
+++ Upudate/Modules/Main/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Upudate
+//
+//  Created by Emre Değirmenci on 4.09.2020.
+//  Copyright © 2020 Ali Emre Degirmenci. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
