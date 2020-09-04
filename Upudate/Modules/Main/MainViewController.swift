5cf7ff6be864a62c8ef83187f1bbfdd3a2af6214
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-09-04 21:51:40 +0300

    Set given image constraints

--- Upudate/Modules/Main/MainViewController.swift
+++ Upudate/Modules/Main/MainViewController.swift
@@ -16,22 +16,30 @@ enum BarButtonTitle: String {
 
 class MainViewController: UIViewController {
 
-    lazy var box = UIImageView()
+    lazy var givenImage = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .red
+        givenImageConstraints()
 
-        //        self.view.addSubview(box)
-        //        box.backgroundColor = .green
-        //        box.image = UIImage(named: "a")
-        //        box.snp.makeConstraints { (make) -> Void in
-        //            make.width.height.equalTo(50)
-        //            make.center.equalTo(self.view)
-        //        }
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: BarButtonTitle.photoLibrary.rawValue, style: .plain, target: self, action: #selector(addTapped))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: BarButtonTitle.camera.rawValue, style: .plain, target: self, action: #selector(addTapped))
+    }
+
+    //MARK: - Given Image Constraints
+    private func givenImageConstraints() {
+        self.view.addSubview(givenImage)
+        givenImage.backgroundColor = .green
+        givenImage.image = UIImage(named: "happy")
+        givenImage.snp.makeConstraints { make in
+            make.right.equalTo(self.view.snp.right).offset(-30)
+            make.left.equalTo(self.view.snp.left).offset(30)
+            make.top.equalTo(self.view).offset(80)
+            make.bottom.equalTo(self.view).offset(-180)
+            make.width.height.equalTo(300)
+        }
     }
 
     @objc func addTapped() {
