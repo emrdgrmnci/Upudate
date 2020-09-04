5a79060efb2fd36bae22e976c9084dee70852fca
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-09-04 18:28:41 +0300

    Initial Commit

---
+++ Upudate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Upudate
+//
+//  Created by Emre Değirmenci on 4.09.2020.
+//  Copyright © 2020 Ali Emre Degirmenci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
